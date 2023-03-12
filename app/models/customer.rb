02e9880145a9a5f7e0447c17feff8fa9201cabd0
EC2 Default User <ec2-user@ip-172-31-36-154.ap-northeast-1.compute.internal>
2023-03-12 00:16:39 +0000

    Add enum gender

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  enum gender: { male: 0, female: 1, others: 2 }
+         
 end
