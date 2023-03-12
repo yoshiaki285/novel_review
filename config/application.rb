bddf1dacee60d0edd83232c8a5a614b1e25c3905
EC2 Default User <ec2-user@ip-172-31-36-154.ap-northeast-1.compute.internal>
2023-03-12 01:13:39 +0000

    Add ja.yml

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module NovelReview
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
   end
